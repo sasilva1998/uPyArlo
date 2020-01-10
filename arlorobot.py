7c17e08b5e2048695b82522bd0038daa6b31f010
Steven <sasilva1998@gmail.com>
2020-01-10 00:32:45 -0500

    reading yet to test

--- arlorobot.py
+++ arlorobot.py
@@ -2,14 +2,6 @@
 from machine import UART
 import utime
 
-# some constants
-DHB10_MAX_MOTOR_PWR = 127
-DT_PREV_ENCODER_CHECK = 40
-LAST_EXCG_STR_LEN = 96
-EXCG_STR_LEN = 96
-LAST_EXCG_TX_MIN = 16
-DEFAULT_TOP_SPEED = 200
-
 # object to control de DHB-10 driver
 class ArloRobot(object):
 	
@@ -21,11 +13,16 @@ class ArloRobot(object):
 			self.uart.write(" ")
 		self.uart.write("\r")
 		tinit=utime.ticks_ms()
-		resp=[]
-		while (utime.ticks_ms()-tinit)<1000: #timeout of 1600us
+		resp=''
+		while (utime.ticks_ms()-tinit)<150: #timeout of 1600us
 			data=self.uart.read(1)
-			if data is not None:
-				resp.append(data)
+			if data is not None and data!=b'\r':
+				resp=resp+data.decode('utf-8')
+
+		if resp is not None:
+			resp=resp.split("xd6")[1].split(" ")
+			resp=[int(i) for i in resp]
+			return resp
 		return resp
 
 	# set up/set down
@@ -37,7 +34,8 @@ class ArloRobot(object):
 		self.baudrate=baudrate
 		self.uart=UART(serialid,self.baudrate)
 		self.uart.init(self.baudrate, bits=8, parity=None, stop=1, txbuf=0,tx=self.tx, rx=self.rx)
-		self.com(["txpin","ch2"])#needed so that reading is possible
+		self.com(["TXPIN","CH2"])#needed so that reading is possible
+		self.com(["DEC"])
 		self.com(["ECHO","ON"])
 		
 
@@ -48,9 +46,15 @@ class ArloRobot(object):
 #-------------------------- movements methods------------------------
 
 	# Turn command
-	# Use not recommended unless firmware is updated
+	# motor_movements corresponds to the amount of encode positions
+	# top_speed to the positions per second
 	def turn(self, motor_movement, top_speed):
 		self.com(["TURN",str(motor_movement),str(top_speed)])
+
+	# arc turns the motors so that the platform moves along the arc of a circle
+	# of a given radius with a speed and an angle
+	def arc(self, radius, top_speed, angle):
+		self.com(["ARC",str(radius),str(top_speed),str(angle)])
 
 	# left/right -> -32767 to 32767
 	# speed -> 1 to 32767
@@ -65,64 +69,83 @@ class ArloRobot(object):
 	def go(self, left, right):
 		self.com(["GO",str(left),str(right)])
 
+	def travel(self,distance,top_speed,angle):
+		self.com(["TRVL",str(distance),str(top_speed),str(angle)])
 
-	# measurements
-	def read_counts_left(self):
-		return self.com(["DIST"])
 
-	def read_counts_right(self):
-		return self.com(["DIST"])
 
-	def read_speed_left(self):
-		return self.com(["SPD"])
+#--------------------------- information methods -----------------------
+	
+	def read_left_counts(self):
+		return self.com(["DIST"])[0]
 
-	def read_speed_right(self):
-		return self.com(["SPD"])
+	def read_right_counts(self):
+		return self.com(["DIST"])[1]
 
-	# communication modes
-	def write_pulse_mode(self):
-		self.com(["PULSE"])
+	def read_left_speed(self):
+		return self.com(["SPD"])[0]
 
-	# information
+	def read_right_speed(self):
+		return self.com(["SPD"])[1]
+
+	def read_head_angle(self):
+		return self.com(["HEAD"])[0]
+
 	def read_firmware_ver(self):
 		return self.com(["VER"])
 
 	def read_hardware_ver(self):
 		return self.com(["HWVER"])
 
-	def read_speedLimit(self):
-		return self.topSpeed
+	def clear_counts(self):
+		return self.com(["RST"])
 
-	#communication
+
+# ---------------------------- communication modes -----------------------
+
+	def write_pulse_mode(self):
+		self.com(["PULSE"])
+
+	def set_lf_mode(self,status):
+		return self.com(["SETLF",str(status)])
+
 	def set_hex_com(self):
 		return self.com(['HEX'])
 
 	def set_dec_com(self):
 		return self.com(['DEC'])
 
-	# configuration
-	def write_config(self, configString, value):
-		pass
+	def set_echo_mode(self,status):
+		return self.com(["ECHO",str(status)])
 
-	def read_config(self, configString):
-		pass
+	def set_verbose_mode(self,status):
+		return self.com(["VERB",str(status)])
 
-	def write_speed_limit(self, countsPerSecond):
-		self.topSpeed=countsPerSecond
+	def set_rx_pin(self,pin):
+		return self.com(["RXPIN",str(pin)])
 
-	def clear_counts(self):
-		self.com(["RST"])
+	def set_tx_pin(self,pin):
+		return self.com(["TXPIN",str(pin)])
 
-	#nonvolatile configuration storage
-	def store_config(self, configString):
-		pass
+	def set_baud_rate(self,baud):
+		return self.com(["BAUD",str(baud)])
 
-	def restore_config(self):
-		self.com(["RESTORE"])
+	def set_pwm_scale(self,scale):
+		return self.com(["SCALE",str(scale)])
 
-	def check_character(self):
-		pass
+	def set_pace(self,pace):
+		return self.com(["PACE",str(pace)])
 
+	def set_hold(self,hold):
+		return self.com(["HOLD",str(baud)])
+
+#-------------------------- closed loop constants ----------------------
+
+	def set_ki_limit(self, limit):
+		return self.com(["KIP",str(limit)])
+
+	def set_ki_decay(self, decay):
+		return self.com(["KIT",str(decay)])
 
 
 def constrain(val, min_val, max_val):
