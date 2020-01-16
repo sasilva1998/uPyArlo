14a814125cc790a4b8230f70de4592f39b001204
sasilva1998 <sasilva1998@gmail.com>
2020-01-16 09:59:50 -0500

    test line

---
+++ tests/lineturn.py
@@ -0,0 +1,13 @@
+from arlorobot import *
+import utime
+
+bot=ArloRobot()
+utime.sleep(1)
+
+while True:
+	bot.turn(100,20)
+	utime.sleep(0.5)
+	bot.move(200,200,20)
+	utime.sleep(0.5)
+	bot.turn(100,20)
+	utime.sleep(0.5)
