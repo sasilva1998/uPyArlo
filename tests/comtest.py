2e3456cccd8f66e252d412783901bb6609dc8911
Your Name <sasilva1998@gmail.com>
2020-08-14 10:32:48 -0500

    formatting

--- tests/comtest.py
+++ tests/comtest.py
@@ -1,6 +1,7 @@
 from arlorobot import *
 import utime
-bot=ArloRobot()
-print('Version de firmware: ',bot.read_firmware_ver())
+
+bot = ArloRobot()
+print("Version de firmware: ", bot.read_firmware_ver())
 utime.sleep(1)
-print('Version de hardware: ',bot.read_hardware_ver())
+print("Version de hardware: ", bot.read_hardware_ver())
--- tests/lineturn.py
+++ tests/lineturn.py
@@ -1,13 +1,13 @@
 from arlorobot import *
 import utime
 
-bot=ArloRobot()
+bot = ArloRobot()
 utime.sleep(1)
 
 while True:
-	bot.turn(100,20)
-	utime.sleep(0.5)
-	bot.move(200,200,20)
-	utime.sleep(0.5)
-	bot.turn(100,20)
-	utime.sleep(0.5)
+    bot.turn(100, 20)
+    utime.sleep(0.5)
+    bot.move(200, 200, 20)
+    utime.sleep(0.5)
+    bot.turn(100, 20)
+    utime.sleep(0.5)
